1d445a0344e31768e55beb07ef77eca93aaaf856
PC <nouracherrad722@gmail.com>
2025-10-18 15:01:00 +0100

    Créer la Gateway Spring cloud Gateway

--- inventory-service/src/main/java/org/sdia/inventoryservice/InventoryServiceApplication.java
+++ inventory-service/src/main/java/org/sdia/inventoryservice/InventoryServiceApplication.java
@@ -1,7 +1,13 @@
 package org.sdia.inventoryservice;
 
+import org.sdia.inventoryservice.Repository.ProductRepository;
+import org.sdia.inventoryservice.entities.Product;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.util.UUID;
 
 @SpringBootApplication
 public class InventoryServiceApplication {
@@ -10,4 +16,29 @@ public class InventoryServiceApplication {
 		SpringApplication.run(InventoryServiceApplication.class, args);
 	}
 
+	@Bean
+	CommandLineRunner commandLineRunner(ProductRepository productRepository) {
+		return args -> {
+		productRepository.save(Product.builder()
+				.id(UUID.randomUUID().toString())
+				.name("computer")
+				.price(3200)
+				.quantity(50)
+				.build());
+		productRepository.save(Product.builder()
+				.id(UUID.randomUUID().toString())
+				.name("tv")
+				.price(320)
+				.quantity(5)
+				.build());
+	productRepository.save(Product.builder()
+			.id(UUID.randomUUID().toString())
+			.name("telephone")
+				.price(1100)
+				.quantity(10)
+				.build());
+
+		};
+
+	}
 }
